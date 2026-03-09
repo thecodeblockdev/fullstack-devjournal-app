653aedc45d3a4254461fa3fcac323a9836763848
code <codeblockdev@gmail.com>
2026-03-09 22:37:05 +0530

    Server setup done

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+export default app;
